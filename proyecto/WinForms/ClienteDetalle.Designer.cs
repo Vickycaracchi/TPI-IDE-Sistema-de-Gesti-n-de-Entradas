79723449232bc0c19c497010dd16506f26eb9ec7
Enrico Reschini <ereschini06@gmail.com>
2025-08-03 21:18:14 -0300

    FormularioCliente iniciado

--- proyecto/WinForms/ClienteDetalle.Designer.cs
+++ proyecto/WinForms/ClienteDetalle.Designer.cs
@@ -28,12 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ClienteDetalle";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            nombreTextBox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            apellidoTextBox = new TextBox();
+            fechaNacTextBox = new TextBox();
+            emailTextBox = new TextBox();
+            numeroTelefonoTextBox = new TextBox();
+            instagramTextBox = new TextBox();
+            button1 = new Button();
+            errorProvider = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(242, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Nombre";
+            label1.Click += label1_Click;
+            // 
+            // nombreTextBox
+            // 
+            nombreTextBox.Location = new Point(441, 122);
+            nombreTextBox.Name = "nombreTextBox";
+            nombreTextBox.Size = new Size(125, 27);
+            nombreTextBox.TabIndex = 1;
+            nombreTextBox.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(242, 172);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Apellido";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(242, 282);
+            label3.Name = "label3";
+            label3.Size = new Size(146, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Fecha de nacimiento";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(242, 79);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 4;
+            label4.Text = "Email";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(242, 221);
+            label5.Name = "label5";
+            label5.Size = new Size(144, 20);
+            label5.TabIndex = 5;
+            label5.Text = "Numero de telefono";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(242, 338);
+            label6.Name = "label6";
+            label6.Size = new Size(75, 20);
+            label6.TabIndex = 6;
+            label6.Text = "Instagram";
+            // 
+            // apellidoTextBox
+            // 
+            apellidoTextBox.Location = new Point(441, 169);
+            apellidoTextBox.Name = "apellidoTextBox";
+            apellidoTextBox.Size = new Size(125, 27);
+            apellidoTextBox.TabIndex = 7;
+            // 
+            // fechaNacTextBox
+            // 
+            fechaNacTextBox.Location = new Point(441, 275);
+            fechaNacTextBox.Name = "fechaNacTextBox";
+            fechaNacTextBox.Size = new Size(125, 27);
+            fechaNacTextBox.TabIndex = 8;
+            // 
+            // emailTextBox
+            // 
+            emailTextBox.Location = new Point(441, 72);
+            emailTextBox.Name = "emailTextBox";
+            emailTextBox.Size = new Size(125, 27);
+            emailTextBox.TabIndex = 9;
+            emailTextBox.TextChanged += emailTextBox_TextChanged;
+            // 
+            // numeroTelefonoTextBox
+            // 
+            numeroTelefonoTextBox.Location = new Point(441, 214);
+            numeroTelefonoTextBox.Name = "numeroTelefonoTextBox";
+            numeroTelefonoTextBox.Size = new Size(125, 27);
+            numeroTelefonoTextBox.TabIndex = 10;
+            // 
+            // instagramTextBox
+            // 
+            instagramTextBox.Location = new Point(441, 335);
+            instagramTextBox.Name = "instagramTextBox";
+            instagramTextBox.Size = new Size(125, 27);
+            instagramTextBox.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(441, 385);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 12;
+            button1.Text = "Enviar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += enviarFormulario;
+            // 
+            // errorProvider
+            // 
+            errorProvider.ContainerControl = this;
+            // 
+            // ClienteDetalle
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1361, 503);
+            Controls.Add(button1);
+            Controls.Add(instagramTextBox);
+            Controls.Add(numeroTelefonoTextBox);
+            Controls.Add(emailTextBox);
+            Controls.Add(fechaNacTextBox);
+            Controls.Add(apellidoTextBox);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(nombreTextBox);
+            Controls.Add(label1);
+            Name = "ClienteDetalle";
+            Text = "ClienteDetalle";
+            ((System.ComponentModel.ISupportInitialize)errorProvider).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox nombreTextBox;
+        private TextBox apellidoTextBox;
+        private TextBox fechaNacTextBox;
+        private TextBox emailTextBox;
+        private TextBox numeroTelefonoTextBox;
+        private TextBox instagramTextBox;
+        private Button button1;
+        private ErrorProvider errorProvider;
     }
 }
--- proyecto/WinForms/ClienteDetalle.cs
+++ proyecto/WinForms/ClienteDetalle.cs
@@ -1,20 +1,170 @@
-﻿using System;
+﻿using DTOs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsForms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WinForms
 {
+    public enum FormMode
+    {
+        Add,
+        Update
+    }
     public partial class ClienteDetalle : Form
     {
+        private ClienteDTO cliente;
+        private FormMode mode;
+        public ClienteDTO Cliente
+        {
+            get { return cliente; }
+            set
+            {
+                cliente = value;
+                this.SetCliente();
+            }
+        }
+        public FormMode Mode
+        {
+            get
+            {
+                return mode;
+            }
+            set
+            {
+                SetFormMode(value);
+            }
+        }
         public ClienteDetalle()
         {
             InitializeComponent();
+
+            Mode = FormMode.Add;
+        }
+
+        private void SetFormMode(FormMode value)
+        {
+            mode = value;
+        }
+        private async void enviarFormulario(object sender, EventArgs e)
+        {
+            if (this.ValidateCliente())
+            {
+                try
+                {
+                    this.Cliente.Email = emailTextBox.Text;
+                    this.Cliente.Nombre = nombreTextBox.Text;
+                    this.Cliente.Apellido = apellidoTextBox.Text;
+                    this.Cliente.NumeroTelefono = numeroTelefonoTextBox.Text;
+                    this.Cliente.FechaNac = DateTime.Parse(fechaNacTextBox.Text);
+                    this.Cliente.Instagram = instagramTextBox.Text;
+
+                    if (this.Mode == FormMode.Update)
+                    {
+                        await ClienteApiClient.UpdateAsync(this.Cliente);
+                    }
+                    else
+                    {
+                        await ClienteApiClient.AddAsync(this.Cliente);
+                    }
+
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+        private void SetCliente()
+        {
+            //this.idTextBox.Text = this.Cliente.Id.ToString();
+            this.emailTextBox.Text = this.Cliente.Email;
+            this.nombreTextBox.Text = this.Cliente.Nombre;
+            this.apellidoTextBox.Text = this.Cliente.Apellido;
+            this.numeroTelefonoTextBox.Text = this.Cliente.NumeroTelefono;
+            this.fechaNacTextBox.Text = this.Cliente.FechaNac.ToString();
+            this.instagramTextBox.Text = this.Cliente.Instagram;
+        }
+        private bool ValidateCliente()
+        {
+            bool isValid = true;
+
+            errorProvider.SetError(emailTextBox, string.Empty);
+            errorProvider.SetError(nombreTextBox, string.Empty);
+            errorProvider.SetError(apellidoTextBox, string.Empty);
+
+            if (this.emailTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(emailTextBox, "El Email es Requerido");
+            }
+            else if (!EsEmailValido(this.emailTextBox.Text))
+            {
+                isValid = false;
+                errorProvider.SetError(emailTextBox, "El formato del Email no es válido");
+            }
+
+            if (this.nombreTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(nombreTextBox, "El Nombre es Requerido");
+            }
+
+            if (this.apellidoTextBox.Text == string.Empty)
+            {
+                isValid = false;
+                errorProvider.SetError(apellidoTextBox, "El Apellido es Requerido");
+            }
+
+            return isValid;
+        }
+
+        private static bool EsEmailValido(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailTextBox_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
