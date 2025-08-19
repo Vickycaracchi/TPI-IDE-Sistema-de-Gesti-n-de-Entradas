201b3c7574b8c61bb9db74571e932b47fec0fe68
Enrico Reschini <ereschini06@gmail.com>
2025-08-19 11:56:30 -0300

    Agregue proyecto API.Clients
    Sigue funcionando como antes. Solo cree el proyeto con los archivos vacios

---
+++ proyecto/API.Clients/ClienteApiClient.cs
@@ -0,0 +1,15 @@
+﻿using DTOs;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace API.Clients
+{
+    public class ClienteApiClient
+    {
+
+    }
+}
---
+++ proyecto/API.Clients/EventoApiClient.cs
@@ -0,0 +1,14 @@
+﻿using DTOs;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace API.Clients
+{
+    internal class EventoApiClient
+    {
+    }
+}
