d18e569753762f192273287563a948c95a4380ca
Enrico Reschini <ereschini06@gmail.com>
2025-10-27 19:50:34 -0300

    estructura inicial 2

---
+++ proyecto/BlazorApp/Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,41 @@
+﻿// BlazorApp/Authentication/CustomAuthStateProvider.cs
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using DTOs;
+
+namespace BlazorApp.Authentication;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private UsuarioDTO? _currentUser;
+
+    public void SetUser(UsuarioDTO? user)
+    {
+        _currentUser = user;
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public void Logout()
+    {
+        _currentUser = null;
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        if (_currentUser == null)
+        {
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, $"{_currentUser.Nombre} {_currentUser.Apellido}"),
+            new Claim(ClaimTypes.Email, _currentUser.Email),
+            new Claim(ClaimTypes.NameIdentifier, _currentUser.Id.ToString()),
+            new Claim("Tipo", _currentUser.Tipo)
+        }, "apiauth");
+
+        return new AuthenticationState(new ClaimsPrincipal(identity));
+    }
+}
--- proyecto/BlazorApp/Program.cs
+++ proyecto/BlazorApp/Program.cs
@@ -1,27 +1,28 @@
-using BlazorApp.Components;
+// BlazorApp/Program.cs
+using BlazorApp.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Solo si usas autenticaci�n personalizada
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
+app.UseRouting();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 
 app.Run();
